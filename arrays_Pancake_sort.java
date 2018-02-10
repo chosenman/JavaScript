f26e07210b18d28ffec2b225810e5adfba258499
chosenman <chosenman2008@gmail.com>
2018-02-09 22:55:58 -0800

    pancacke sort

--- arrays_Pancake_sort.java
+++ arrays_Pancake_sort.java
@@ -1,52 +1,52 @@
 import java.io.*;
 import java.util.*; 
 
-class Solution {
-
-  static int[] pancakeSort(int[] arr) {
-
+// "static void main" must be defined in a public class.
+public class Main {
     
-    // we are gradually reducing range
-    for(int length = arr.length; length >=0; length--) {
-      
-      int biggestValue = arr[0];
-      int biggestIndex = 0;
-      
-      // looking for biggest in current range
-      for( int i = 0; i < length; i++){ // this range will be reduced (length)
-        if(arr[i] > biggestValue) {
-          biggestValue = arr[i];
-          biggestIndex = i;
-        }
-      }
-      arr = flip(arr,biggestIndex); // we put BIGGEST in front
-      arr = flip(arr, length-1 ); // we need it to don't tuch already flipped previous BIGGEST ONES
-      
-    }
-    
-    return arr;
-    
-  }
+    // pancacke sort
+          static int[] pancakeSort(int[] arr) {
+            // we are gradually reducing range
+            for(int length = arr.length; length >=0; length--) {
+              int biggestValue = arr[0];
+              int biggestIndex = 0;
+              // looking for biggest in current range
+              for( int i = 0; i < length; i++){ // this range will be reduced (length)
+                if(arr[i] > biggestValue) {
+                  biggestValue = arr[i];
+                  biggestIndex = i;
+                }
+              }
+              // we put BIGGEST in front
+              arr = flip(arr,biggestIndex); 
+              // we need it to don't tuch already flipped previous BIGGEST ONES
+              arr = flip(arr, length-1 ); 
+            }
+            return arr;
+          }
   
   // flip function flips order in given range (first k elems) of array
-    static int[] flip(int [] arr , int k ){
-      int endIndex = k;
-      for(int i = 0 ; i <=  k ; i ++, endIndex--){
+        static int[] flip(int [] arr , int k ){
+          int endIndex = k;
+          for(int i = 0 ; i <=  k ; i ++, endIndex--){
 
-        if( endIndex != i && endIndex > i ) {
-          int swap = arr[i];
-          arr[i] = arr[endIndex];
-          arr[endIndex] = swap;
-        } else {
-          break;
+            if( endIndex != i && endIndex > i ) {
+              int swap = arr[i];
+              arr[i] = arr[endIndex];
+              arr[endIndex] = swap;
+            } else {
+              break;
+            }
+
+          }
+          return arr;
         }
+    
+    
+    // execution
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 4, 3, 2};
+        System.out.println( Arrays.toString( pancakeSort(arr) )  );
 
-      }
-      return arr;
     }
-
-  public static void main(String[] args) {
-
-  }
-
 }
