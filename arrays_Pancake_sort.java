f92960a5665c78fb6ee7c038a3079a469562ccf3
chosenman <chosenman2008@gmail.com>
2018-02-09 22:45:16 -0800

    made pancacke sort on Java

---
+++ arrays_Pancake_sort.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*; 
+
+class Solution {
+
+  static int[] pancakeSort(int[] arr) {
+
+    
+    // we are gradually reducing range
+    for(int length = arr.length; length >=0; length--) {
+      
+      int biggestValue = arr[0];
+      int biggestIndex = 0;
+      
+      // looking for biggest in current range
+      for( int i = 0; i < length; i++){ // this range will be reduced (length)
+        if(arr[i] > biggestValue) {
+          biggestValue = arr[i];
+          biggestIndex = i;
+        }
+      }
+      arr = flip(arr,biggestIndex); // we put BIGGEST in front
+      arr = flip(arr, length-1 ); // we need it to don't tuch already flipped previous BIGGEST ONES
+      
+    }
+    
+    return arr;
+    
+  }
+  
+  static int[] flip(int [] arr , int k ){
+    int endIndex = k;
+    for(int i = 0 ; i <=  k ; i ++, endIndex--){
+
+      if( endIndex != i && endIndex > i ) {
+        int swap = arr[i];
+        arr[i] = arr[endIndex];
+        arr[endIndex] = swap;
+      } else {
+        break;
+      }
+
+    }
+    return arr;
+  }
+
+  public static void main(String[] args) {
+
+  }
+
+}
