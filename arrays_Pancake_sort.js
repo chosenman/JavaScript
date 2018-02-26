7e47c4b99b7403af31dfda9b30e6283731ffc081
chosenman <chosenman2008@gmail.com>
2018-02-26 12:52:45 -0800

    update

---
+++ arrays_Pancake_sort.js
@@ -0,0 +1,46 @@
+
+
+
+function pancakeSort(arr){
+
+	for (var length = arr.length; length >= 0; length--) {
+		var biggestValue = arr[0];
+		var biggestIndex = 0;
+		// looking for biggest in the current range
+		for (var i = 0; i < length; i++) {
+			if(arr[i] > biggestValue){
+				biggestValue = arr[i];
+				biggestIndex = i;
+			}
+		}
+		// we put BIGGEST in front
+		arr = flip(arr, biggestIndex);
+		// 
+		arr = flip(arr, length-1);
+	}
+	return arr;
+}
+
+	// flip function
+	function flip(arr, k){
+		var endIndex = k;
+
+		for (var i = 0; i <= k; i++; endIndex--) {
+			if( endIndex != i && endIndex >i ){
+				var swap = arr[i];
+				arr[i] = arr[endIndex];
+				arr[endIndex] = swap;
+			} else {
+				break;
+			}
+		}
+	}
+
+	console.log( pancakeSort([1, 5, 4, 3, 2]) );
+
+
+
+
+
+
+
